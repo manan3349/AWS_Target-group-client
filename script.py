9e072c042c4d46ed5b9bb1f700380404a5f2811f
Manan Jain <63852645+manan3349@users.noreply.github.com>
2021-08-04 12:34:56 +0530

    Create script.py

---
+++ script.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import sys
+import boto3
+
+if len(sys.argv) != 4:
+	print("Usage:")
+	print(f"{' '*4} {sys.argv[0]} [PORT] [VPC] [TAGNAME Key:Value] ")
+	sys.exit(1)
+
+PORT = int(sys.argv[1])
+KEY,VALUE = str(sys.argv[3]).split(":")
+VPC = str(sys.argv[2])
+temp = {}
+instanceids= []
+tg_name = 'New-target-grp'
+
+
+alb = boto3.client('elbv2')
+client = boto3.client('ec2')
+
+response = client.describe_instances(
+    Filters=[
+        {
+            'Name': str('tag:'+KEY),
+            'Values': [
+                VALUE,
+            ]
+        },
+    ]).get('Reservations', [])
+instances = sum([[i for i in r['Instances']]for r in response], [])
+for instance in instances:
+	temp['Id']=instance['InstanceId']
+	instanceids.append(temp.copy())
+
+response = alb.create_target_group( Name=tg_name, Protocol='HTTP', Port=PORT, VpcId=VPC,TargetType='instance')
+
+arn = response['TargetGroups'][0]['TargetGroupArn']
+
+response = alb.register_targets(TargetGroupArn=arn,Targets=instanceids)
+if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+	temp =[i["Id"] for i in instanceids]
+	print(f"Completed: New target group named {tg_name} created and targets {temp} attached to it.")
