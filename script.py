823bfdf3eded4b6037eb432fde2659f24abb48c0
Manan Jain <63852645+manan3349@users.noreply.github.com>
2021-08-04 16:10:55 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,50 +4,52 @@ import sys
 import boto3
 
 if len(sys.argv) != 5:
-	print("Usage:")
-	print(f"{' '*4} {sys.argv[0]} [PORT] [VPC] [TAGNAME Key:Value] [LISTENERARN]")
-	sys.exit(1)
+print("Usage:")
+print(f"{' '*4} {sys.argv[0]} [PORT] [VPC] [TAGNAME Key:Value] [LISTENERARN]")
+sys.exit(1)
 
 PORT = int(sys.argv[1])
 KEY,VALUE = str(sys.argv[3]).split(":")
 VPC = str(sys.argv[2])
 temp = {}
 instanceids= []
-tg_name = 'Newest-target-grp'
+tg_name = 'New-target-grp'
 ls_arn = str(sys.argv[4])
 
 alb = boto3.client('elbv2',region_name='us-east-1')
 client = boto3.client('ec2',region_name='us-east-1')
 
 response = client.describe_instances(
-    Filters=[
-        {
-            'Name': str('tag:'+KEY),
-            'Values': [
-                VALUE,
-            ]
-        },
-    ]).get('Reservations', [])
+Filters=[
+{
+'Name': str('tag:'+KEY),
+'Values': [
+VALUE,
+]
+},
+]).get('Reservations', [])
 instances = sum([[i for i in r['Instances']]for r in response], [])
 for instance in instances:
-	temp['Id']=instance['InstanceId']
-	instanceids.append(temp.copy())
+temp['Id']=instance['InstanceId']
+instanceids.append(temp.copy())
 
 response = alb.create_target_group( Name=tg_name, Protocol='HTTP', Port=PORT, VpcId=VPC,TargetType='instance')
 arn = response['TargetGroups'][0]['TargetGroupArn']
 
 response = alb.modify_listener(
-	ListenerArn=ls_arn,
-    DefaultActions=[
-        {
-            'TargetGroupArn': arn,
-            'Type': 'forward',
-        },
-    ],
-    Port=80,
-    Protocol='HTTP')
+ListenerArn=ls_arn,
+DefaultActions=[
+{
+'TargetGroupArn': arn,
+'Type': 'forward',
+},
+],
+Port=80,
+Protocol='HTTP')
 
 response = alb.register_targets(TargetGroupArn=arn,Targets=instanceids)
 if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-	temp =[i["Id"] for i in instanceids]
-	print(f"Completed: New target group named {tg_name} created and targets {temp} attached to it.")
+temp =[i["Id"] for i in instanceids]
+print(f"Completed: New target group named {tg_name} created and targets {temp} attached to it.")
+```
+A message was deleted in Google Chat (https://chat.google.com/dm/xgEDpwAAAAE).
